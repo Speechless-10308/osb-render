fix the stupid computation bug for origin. now it is correct, maybe..

src/managers.py
@@ -1,16 +1,19 @@
 import os
 from typing import Dict
 from PIL import Image, ImageOps
+import cv2
+from cv2.typing import MatLike
+import numpy as np
 
 
 class AssetLoader:
     def __init__(self, base_path: str):
         self.base_path = base_path
-        self.cache: Dict[str, Image.Image] = {}
+        self.cache: Dict[str, MatLike] = {}
 
-        self.placeholder = Image.new("RGBA", (1, 1), (0, 0, 0, 0))
+        self.placeholder = None
 
-    def load_image(self, filepath: str) -> Image.Image:
+    def load_image(self, filepath: str, method: str = "cv2") -> MatLike | Image.Image:
         # normalize path
         filepath = filepath.strip('"').replace("\\", os.sep)
         full_path = os.path.join(self.base_path, filepath)
@@ -24,9 +27,19 @@ class AssetLoader:
             return self.placeholder
 
         try:
-            img = Image.open(full_path).convert("RGBA")
-            self.cache[filepath] = img
-            return img
+            if method == "cv2":
+                img_array = np.fromfile(full_path, dtype=np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_UNCHANGED)
+
+                if img.shape[2] == 3:
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+
+                self.cache[filepath] = img
+                return img
+            elif method == "pil":
+                img = Image.open(full_path).convert("RGBA")
+                self.cache[filepath] = img
+                return img
         except Exception as e:
             print(f"Error loading image {full_path}: {e}")
             self.cache[filepath] = self.placeholder